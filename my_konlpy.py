d8c683da56bc5ca4189e6121fc3002c460807869
yukiriru <165104565+yukiriru@users.noreply.github.com>
2024-05-29 02:19:38 +0900

    불용어도 추가 제외하도록 변경

--- my_konlpy.py
+++ my_konlpy.py
@@ -3,7 +3,13 @@ from konlpy.tag import Okt
 okt = Okt()
 
 def save_nouns(text):
+
     nouns_list = okt.nouns(text)
+
+    with open('stopwords.txt', 'r', encoding='utf-8') as file:
+        stopwords = file.readlines()
+    stopwords = [word.strip() for word in stopwords]
+    nouns_list = [word for word in nouns_list if word not in stopwords]
     with open('save_text.txt', 'a', encoding='utf-8') as file:
         for noun in nouns_list:
             file.write(noun + '\n')
