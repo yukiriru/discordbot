96ac51bf173e48985b1ce2fe673d4a38c8909023
yukiriru <165104565+yukiriru@users.noreply.github.com>
2024-05-29 01:11:22 +0900

    날씨 code error edit

--- main.py
+++ main.py
@@ -46,10 +46,12 @@ class MyClient(discord.Client):
             outcome = '앞면' if random.choice([True, False]) else '뒷면'
             await channel.send('동전 뒤집기 결과: {}'.format(outcome))
         elif content.startswith('날씨 '):
-            city = content.split(' ', 1)[1]
+            if ' ' in content:
+                city = content.split(' ', 1)[1]
+            else:
+                city = '서울'
             weather = self.get_weather(city)
             await channel.send(weather)
-
         elif content.startswith('remind '):
             try:
                 parts = content.split(' ', 2)
